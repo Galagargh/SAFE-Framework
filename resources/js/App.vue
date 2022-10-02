9e4372038dcb912c89e0c97c897f62e2a762aaed
Galagargh <48443065+Galagargh@users.noreply.github.com>
2022-10-02 19:59:19 +0100

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="test text-3xl text-purple-600 font-bold">How to Install Tailwind CSS 3 in Laravel 9 Using Vite 3 - LaraMatic</h1>
+        <p class="text-green-500 text-3xl">This text should be green</p>
+    </div>
+</template>
